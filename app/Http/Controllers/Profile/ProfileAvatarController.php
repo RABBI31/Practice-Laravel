4d7540e3fd9626794010917f44315567977b9292
Md.Ali Rabbi <56753742+RABBI31@users.noreply.github.com>
2023-06-16 01:16:48 +0600

    Add-delete-previous-image-when-update new-one

--- app/Http/Controllers/Profile/ProfileAvatarController.php
+++ app/Http/Controllers/Profile/ProfileAvatarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProfileAvatarController extends Controller
 {
@@ -11,7 +12,11 @@ class ProfileAvatarController extends Controller
 $request->validate([
 'avatar'=>'image',
 ]);
-            $path = $request->file('avatar')->store('avatars','public');
+            // $path = $request->file('avatar')->store('avatars','public');
+$path=Storage::disk('public')->put('avatars', $request->file('avatar'));
+if($oldavatar = $request->user()->avatar){
+Storage::disk('public')->delete($oldavatar);
+}
             auth()->user()->update(['avatar' => "$path"]);
             return redirect(route('profile.edit'))->with('message', 'Avatar is updated');
         }
